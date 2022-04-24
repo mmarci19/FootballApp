4cae785508065185de6e1f3edc8bdd3524ee30da
Molnár Márton <47712831+mmarci19@users.noreply.github.com>
2022-04-24 02:43:33 +0200

    Update TeamService.kt

--- app/src/main/java/com/example/footballapp/network/TeamService.kt
+++ app/src/main/java/com/example/footballapp/network/TeamService.kt
@@ -8,12 +8,12 @@ import retrofit2.http.Query
 
 interface TeamService {
 
-@Headers("X-RapidAPI-Key: 3af0cfc0e3msh1751c32679b8f64p1f6954jsn54fb0872207f")
-@GET("teams")
+@Headers("X-RapidAPI-Key: <key>")
+  @GET("teams")
 suspend fun fetchTeamList(@Query("season") season: String, @Query("code") code: String, @Query("type") type: String): Call<List<Team>>
 
-@Headers("X-RapidAPI-Key: 3af0cfc0e3msh1751c32679b8f64p1f6954jsn54fb0872207f")
-@GET("teams/statistics")
+@Headers("X-RapidAPI-Key: <key>")
+  @GET("teams/statistics")
 suspend fun fetchTeamStats(@Query("season") season: String, @Query("code") code: String, @Query("type") type: String, @Query("id") id_ : String): Call<Team>
 
 }
