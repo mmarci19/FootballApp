315b3b56f961e174747172da0ca310998065344a
Molnár Márton <molnarmarton191@gmail.com>
2022-05-06 20:33:25 +0200

    leagues and teams list

---
+++ app/src/main/java/com/example/footballapp/ui/teams/TeamAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.footballapp.ui.leagues
+
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.footballapp.R
+import com.example.footballapp.model.league.League
+import com.example.footballapp.model.team.Team
+import com.example.footballapp.ui.details.DetailsActivity
+import com.example.footballapp.ui.teams.TeamsActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.league_row.view.*
+
+class TeamAdapter(private val teams: List<Team>) : RecyclerView.Adapter<TeamAdapter.TeamHolder>()  {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamAdapter.TeamHolder {
+        val inflatedView = parent.inflate(R.layout.league_row, false)
+
+        return TeamHolder(inflatedView)
+    }
+
+
+    override fun onBindViewHolder(holder: TeamAdapter.TeamHolder, position: Int) {
+        Log.d("ADAPTER",position.toString())
+        Log.d("ADAPTER",teams[position].toString())
+        val itemTeam = teams[position]
+        holder.bindTeam(itemTeam)
+    }
+
+
+    override fun getItemCount() = teams.size
+
+
+    class TeamHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        //2
+        private var view: View = v
+        private var team: Team? = null
+
+
+        //3
+        init {
+            v.setOnClickListener(this)
+
+
+        }
+
+        //4
+        override fun onClick(v: View) {
+            val context = itemView.context
+
+
+            val intent = Intent(context, DetailsActivity::class.java)
+            // To pass any data to next activity
+            intent.putExtra("team_id", this.team?.id.toString())
+            Log.d("INTENT",intent.getStringExtra("team_id").toString())
+            // start your next activity
+            context.startActivity(intent)
+        }
+        fun bindTeam(team: Team) {
+            Log.d("HOLDER",team.name.toString())
+            view.itemName.text = team?.name
+            Picasso.get().load(team?.logo).into(view.itemImage)
+            this.team = team
+
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/footballapp/ui/teams/TeamsActivity.kt
+++ app/src/main/java/com/example/footballapp/ui/teams/TeamsActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.footballapp.databinding.LeaguesListBinding
 import com.example.footballapp.databinding.TeamsBinding
 import com.example.footballapp.model.league.League
 import com.example.footballapp.model.team.Team
 import com.example.footballapp.ui.details.DetailsActivity
+import com.example.footballapp.ui.leagues.LeagueAdapter
 import com.example.footballapp.ui.leagues.LeagueViewModel
+import com.example.footballapp.ui.leagues.TeamAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -18,7 +22,8 @@ import kotlinx.coroutines.runBlocking
 @AndroidEntryPoint
 class TeamsActivity : ComponentActivity() {
 private lateinit var binding: TeamsBinding
-
+    private lateinit var adapter: TeamAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 internal val teamsViewModel: TeamViewModel by viewModels()
 
 override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,14 +42,9 @@ override fun onCreate(savedInstanceState: Bundle?) {
         teamsList = teams.first()
     }
 
-    binding.textView2.text = teamsList[0].name
-    binding.button.setOnClickListener {
-        val intent = Intent(this, DetailsActivity::class.java)
-        // To pass any data to next activity
-        intent.putExtra("league_id", league_id)
-        intent.putExtra("team_id",651)
-        // start your next activity
-        startActivity(intent)
-    }
+    linearLayoutManager = LinearLayoutManager(this)
+    binding.recyclerView.layoutManager = linearLayoutManager
+    adapter = TeamAdapter(teamsList)
+    binding.recyclerView.adapter = adapter
 }
 }
